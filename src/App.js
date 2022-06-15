41b16de52ad738569b53a257e10bb5980d11c9bc
charlenetormes <charlene@inspireteam.com>
2022-06-15 16:03:31 +0800

    Added left side for hero images and addtl npm libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Hero from './Hero'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /**
+     *  
+     *  PLEASE JUST DELETE THIS BOILERPLATE WELCOME CODE I MADE
+     *  
+     * **/
+     <div className="w-full h-auto flex flex-col bg-primary">
+         <Hero></Hero>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
